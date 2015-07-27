e0fe4444203e893120603989cf5300a5f9d4b353
zhibirc <zhibirc@dot.com>
2015-07-27 15:40:53 +0300

    Add some tests.

--- test/test.js
+++ test/test.js
@@ -1,2 +1,6 @@
-var expect = chai.expect;
+var assert = require('chai').assert;
 
+assert(typeof jupiterOrm != undefined, 'You must import jupiterOrm.');
+assert(typeof knex != undefined, 'You must include Knex in your code.');
+// Use this hack because 'constructor.name' may throw TypeError.
+assert(Object.prototype.toString.call(jupiterOrm).slice(8, -1) == 'Object', 'You must import jupiterOrm.');
