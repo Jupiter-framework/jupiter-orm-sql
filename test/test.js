686b81536af65a9b12d5117b06e0ccd79c704b95
zhibirc <zhibirc@dot.com>
2015-07-27 16:40:09 +0300

    Add tests. Reduce .gitignore.

--- test/test.js
+++ test/test.js
@@ -4,3 +4,5 @@ assert(typeof jupiterOrm != undefined, 'You must import jupiterOrm.');
 assert(typeof knex != undefined, 'You must include Knex in your code.');
 // Use this hack because 'constructor.name' may throw TypeError.
 assert(Object.prototype.toString.call(jupiterOrm).slice(8, -1) == 'Object', 'You must import jupiterOrm.');
+assert(Object.prototype.toString.call(jupiterOrmSql.cache).slice(8, -1) == 'Object', 'Set cache property to object literal for future usage.');
+assert(typeof jupiterOrmSql['Fabric'] == 'function', 'Create Fabric method in adapter.');
