645bab921c038a34477af5ffa19219879cb6c9b0
zhibirc <zhibirc@dot.com>
2015-07-28 17:17:10 +0300

    Change README.md

--- tests/test.js
+++ tests/test.js
@@ -3,10 +3,14 @@ var chai = require('chai'),
 	expect = chai.expect;
 
 assert(typeof jupiterOrm != undefined, 'You must import jupiterOrm.');
+
 assert(typeof knex != undefined, 'You must include Knex in your code.');
+
 // Use this hack because 'constructor.name' may throw TypeError.
 assert(Object.prototype.toString.call(jupiterOrm).slice(8, -1) == 'Object', 'You must import jupiterOrm.');
+
 assert(Object.prototype.toString.call(jupiterOrmSql.cache).slice(8, -1) == 'Object', 'Set cache property to object literal for future usage.');
+
 assert(typeof jupiterOrmSql['Fabric'] == 'function', 'Create Fabric method in adapter.');
 
 expect(jupiterOrmSql.Fabric()).to.have.property('Fabric', 'exec');
