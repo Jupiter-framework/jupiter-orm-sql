107f0b2a4c1775f41b02a06a1fa2cfa3f5a6b459
zhibirc <zhibirc@dot.com>
2015-07-29 10:51:19 +0300

    Modify code of the adapter.

--- src/jupiter-orm-sql.js
+++ src/jupiter-orm-sql.js
@@ -6,11 +6,23 @@ var jupiterOrmSql = require('knex');
 export function Fabric(options) {
 	const query = {};
 	
+	/**
+	 *	Connect to database.
+	 */
 	function connect() {
 		return jupiterOrmSql({
 			client: 'mysql',
 			connection: options
 		});
 	}
+	
+	/**
+	 *	Construct queries to database.
+	 */
+	jupiterOrmSql.query = function () {
+		
+			
+	};
+	
 	return jupiterOrmSql;
 }
